02286e194b8bb55e325c3f7e58a416038641ae82
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-06-02 13:18:32 +0300

    Added constants

--- app/src/main/java/com/koniukhov/brushbliss/util/Constants.kt
+++ app/src/main/java/com/koniukhov/brushbliss/util/Constants.kt
@@ -3,3 +3,11 @@ package com.koniukhov.brushbliss.util
 const val DEFAULT_COLOR = -16777216
 const val DEFAULT_ALPHA = 255
 const val DEFAULT_STROKE_WIDTH = 4f
+
+const val BASE_BRUSH_STROKE_FROM = 1
+const val BASE_BRUSH_STROKE_TO = 10
+const val BASE_BRUSH_STROKE_STEP = 1
+
+const val BASE_BRUSH_ALPHA_FROM = 0
+const val BASE_BRUSH_ALPHA_TO = 255
+const val BASE_BRUSH_ALPHA_STEP = 1
