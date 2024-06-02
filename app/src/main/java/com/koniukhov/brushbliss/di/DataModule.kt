2a3d2804a04f891fcf8c0fa3fc768ce7def29c15
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-06-02 13:20:13 +0300

    Added di

---
+++ app/src/main/java/com/koniukhov/brushbliss/di/DataModule.kt
@@ -0,0 +1,20 @@
+package com.koniukhov.brushbliss.di
+
+import android.content.Context
+import com.koniukhov.brushbliss.data.UserSettingsManager
+import com.koniukhov.brushbliss.data.UserSettingsManager.Companion.datastore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+
+    @Provides
+    fun provideUserSettingsManager(@ApplicationContext context: Context): UserSettingsManager{
+        return UserSettingsManager(context.datastore)
+    }
+}
