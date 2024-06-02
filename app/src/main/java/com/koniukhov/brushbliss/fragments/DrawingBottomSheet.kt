d09f31b1a43f8e97b2ee907d4afd65d58b868865
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-06-02 13:49:44 +0300

    Changed behaviour

--- app/src/main/java/com/koniukhov/brushbliss/fragments/DrawingBottomSheet.kt
+++ app/src/main/java/com/koniukhov/brushbliss/fragments/DrawingBottomSheet.kt
@@ -8,10 +8,14 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.koniukhov.brushbliss.databinding.DrawingBottomSheetContentBinding
+import com.koniukhov.brushbliss.dialogs.BaseBrushDialogFragment
+import com.koniukhov.brushbliss.util.BASE_BRUSH_DIALOG_TAG
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DrawingBottomSheet : BottomSheetDialogFragment() {
     private var _binding: DrawingBottomSheetContentBinding? = null
-    private val  binding get() = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +23,7 @@ class DrawingBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View{
         _binding =  DrawingBottomSheetContentBinding.inflate(inflater, container, false)
+        initClickListeners()
         return binding.root
     }
 
@@ -34,7 +39,15 @@ class DrawingBottomSheet : BottomSheetDialogFragment() {
         }
     }
 
-    companion object {
-        const val TAG = "DrawingBottomSheet"
+    private fun initClickListeners(){
+        binding.baseBrush.setOnClickListener{
+            val dialog = BaseBrushDialogFragment()
+            dialog.show(parentFragmentManager, BASE_BRUSH_DIALOG_TAG)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
