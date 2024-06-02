addad1cb877604e1f10f41df21c878989f41967b
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-06-02 13:16:20 +0300

    Added di

--- app/src/main/java/com/koniukhov/brushbliss/MainActivity.kt
+++ app/src/main/java/com/koniukhov/brushbliss/MainActivity.kt
@@ -3,7 +3,9 @@ package com.koniukhov.brushbliss
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
