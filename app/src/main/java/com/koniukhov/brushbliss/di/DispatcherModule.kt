89d47213a0b8a3e2132c225262e787978de78747
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-06-02 13:21:47 +0300

    Added di

---
+++ app/src/main/java/com/koniukhov/brushbliss/di/DispatcherModule.kt
@@ -0,0 +1,21 @@
+package com.koniukhov.brushbliss.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DispatcherModule {
+    @Provides
+    fun provideDispatcherIO(): CoroutineDispatcher{
+        return Dispatchers.IO
+    }
+
+    @Provides fun provideDispatcherMain(): CoroutineDispatcher{
+        return Dispatchers.Main
+    }
+}
