9afd4dcd3b1d920f507be4ed0f83ed3ab205a5b8
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-06-04 18:07:09 +0300

    Added constants

--- app/src/main/java/com/koniukhov/brushbliss/util/Constants.kt
+++ app/src/main/java/com/koniukhov/brushbliss/util/Constants.kt
@@ -1,6 +1,8 @@
 package com.koniukhov.brushbliss.util
 
-const val DEFAULT_COLOR = -16777216
+import android.graphics.Color
+
+const val DEFAULT_COLOR = Color.BLACK
 const val DEFAULT_ALPHA = 255
 const val DEFAULT_STROKE_WIDTH = 4f
 
@@ -8,9 +10,14 @@ const val BASE_BRUSH_STROKE_FROM = 1
 const val BASE_BRUSH_STROKE_TO = 10
 const val BASE_BRUSH_STROKE_STEP = 1
 
-const val BASE_BRUSH_ALPHA_FROM = 0
-const val BASE_BRUSH_ALPHA_TO = 255
-const val BASE_BRUSH_ALPHA_STEP = 1
+const val BYTE_FROM = 0
+const val BYTE_TO = 255
+const val STEP_BY_ONE = 1
+
+const val HEX_LENGTH = 6
+
+const val HEXADECIMAL_RADIX = 16
 
 const val BASE_BRUSH_DIALOG_TAG = "BaseBrushDialogFragment"
 const val DRAWING_BOTTOM_SHEET_TAG = "DrawingBottomSheet"
+const val COLOR_PICKER_DIALOG_TAG = "ColorPickerDialogFragment"
